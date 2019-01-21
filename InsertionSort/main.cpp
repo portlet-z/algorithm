31cf37892e7c93ed2d8298adbdfd03222b8bd05e
张新征 <programer.email@gmail.com>
2019-01-22 00:02:27 +0800

    InsertionSort 优化

--- InsertionSort/main.cpp
+++ InsertionSort/main.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 
 template<typename T>
+//插入排序对于近乎有序的数组，性能非常好，因为第二层循环可以提前终止
+//对于完全有序的数组，插入排序时间复杂度进化成为 O(n)
 void insertionSort(T arr[], int n) {
     //从第一个元素开始，第0个元素已经有序了
     for(int i=1; i<n; i++) {
@@ -18,9 +20,17 @@ void insertionSort(T arr[], int n) {
 //           }
 //       }
         //与上面等价
-        for(int j=i; j>0 && arr[j] < arr[j-1]; j--) {
-            swap(arr[j], arr[j-1]);
+//        for(int j=i; j>0 && arr[j] < arr[j-1]; j--) {
+//            swap(arr[j], arr[j-1]);
+//        }
+        //改进，变交换操作为赋值操作
+        T e = arr[i];
+        // j 保存元素e 应该插入的位置
+        int j;
+        for(j=i; j>0 && arr[j-1] > e; j--) {
+            arr[j] = arr[j-1];
         }
+        arr[j] = e;
     }
 }
 
@@ -35,5 +45,14 @@ int main() {
 
     delete[] arr;
     delete[] arr2;
+
+    int *a = SortTestHelper::generateNearlyOrderedArray(n,10);
+    int *a2 = SortTestHelper::copyIntArray(arr, n);
+
+    SortTestHelper::testSort("Insertion Sort", insertionSort, a, n);
+    SortTestHelper::testSort("Selection Sort", selectionSort, a2, n);
+
+    delete[] a;
+    delete[] a2;
     return 0;
 }
