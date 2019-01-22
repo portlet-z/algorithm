160c59753b762b5aac02a92dd55e094382236e8f
张新征 <programer.email@gmail.com>
2019-01-22 09:05:04 +0800

    add QuickSort

---
+++ QuickSort/InsertionSort.h
@@ -0,0 +1,55 @@
+//
+// Created by 张新征 on 2019-01-22.
+//
+
+#ifndef MERGESORT_INSERTIONSORT_H
+#define MERGESORT_INSERTIONSORT_H
+
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+//插入排序对于近乎有序的数组，性能非常好，因为第二层循环可以提前终止
+//对于完全有序的数组，插入排序时间复杂度进化成为 O(n)
+void insertionSort(T arr[], int n) {
+    //从第一个元素开始，第0个元素已经有序了
+    for(int i=1; i<n; i++) {
+        //寻找元素 arr[i] 合适的插入位置
+        //j 与前一个元素比较， j 不能等于0，最多考虑到 j=1
+//       for(int j=i; j>0; j--) {
+//           if(arr[j] < arr[j-1]) {
+//               swap(arr[j], arr[j-1]);
+//           } else {
+//               break;
+//           }
+//       }
+        //与上面等价
+//        for(int j=i; j>0 && arr[j] < arr[j-1]; j--) {
+//            swap(arr[j], arr[j-1]);
+//        }
+        //改进，变交换操作为赋值操作
+        T e = arr[i];
+        // j 保存元素e 应该插入的位置
+        int j;
+        for(j=i; j>0 && arr[j-1] > e; j--) {
+            arr[j] = arr[j-1];
+        }
+        arr[j] = e;
+    }
+}
+
+template<typename T>
+void insertionSort(T arr[], int l, int r) {
+    for(int i=l+1; i<=r; i++) {
+        T e = arr[i];
+        int j;
+        for(j=i; j>l && arr[j-1] > e; j--) {
+            arr[j] = arr[j-1];
+        }
+        arr[j] = e;
+    }
+    return;
+}
+
+#endif //MERGESORT_INSERTIONSORT_H
---
+++ QuickSort/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "SortTestHelper.h"
+#include "MergeSort.h"
+
+using namespace std;
+
+//对 arr[l...r]部分进行 partition 操作
+//返回 p,使得 arr[l...p-1] < arr[p]; arr[p+1...r] > arr[p]
+template<typename T>
+int __partition(T arr[], int l, int r) {
+    T v = arr[l];
+    // arr[l+1...j] < v; arr[j+1...i) > v
+    int j = l;
+    for(int i=l+1; i<=r; i++) {
+        if(arr[i] < v) {
+//            swap(arr[j+1], arr[i]);
+//            j++;
+            //和上面两行代码等价
+            swap(arr[++j], arr[i]);
+        }
+    }
+    swap(arr[l], arr[j]);
+    return j;
+}
+
+//对 arr[l...r]部分进行快速排序
+template<typename T>
+void __quickSort(T arr[], int l, int r){
+    if(l >= r) {
+        return;
+    }
+    int p = __partition(arr, l, r);
+    __quickSort(arr, l, p-1);
+    __quickSort(arr, p+1, r);
+}
+
+template<typename T>
+void quickSort(T arr[], int n) {
+    __quickSort(arr, 0, n);
+}
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    int n = 2000000;
+    int* arr1 = SortTestHelper::generateRandomArray(n, 0, n);
+    int* arr2 = SortTestHelper::copyIntArray(arr1, n);
+
+    SortTestHelper::testSort("MergeSort", mergeSort, arr1, n);
+    SortTestHelper::testSort("QuickSort", quickSort, arr2, n);
+
+    delete[] arr1;
+    delete[] arr2;
+    return 0;
+}
