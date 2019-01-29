7a0bbb1ec6ce276082e7d29504672edb4771d88e
张新征 <programer.email@gmail.com>
2019-01-29 08:25:19 +0800

    原地堆排序

--- HeapSort/main.cpp
+++ HeapSort/main.cpp
@@ -26,6 +26,34 @@ void heapSort2(T arr[], int n) {
     }
 }
 
+template <typename T>
+void __shiftDown(T arr[], int n, int k) {
+    while (2*k+1 < n) {
+        int j = 2*k + 1;
+        if(j + 1 < n && arr[j+1] > arr[j]) {
+            j++;
+        }
+        if(arr[k] >= arr[j]) {
+            break;
+        }
+        swap(arr[k], arr[j]);
+        k = j;
+    }
+}
+
+template <typename T>
+void heapSort(T arr[], int n) {
+    //heapify
+    for(int i=(n-1)/2; i>=0; i--) {
+        __shiftDown(arr, n, i);
+    }
+
+    for(int i=n-1; i>0; i--) {
+        swap(arr[0], arr[i]);
+        __shiftDown(arr, i, 0);
+    }
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
     int n = 1000000;
@@ -34,18 +62,21 @@ int main() {
     int* arr3 = SortTestHelper::copyIntArray(arr1, n);
     int* arr4 = SortTestHelper::copyIntArray(arr1, n);
     int* arr5 = SortTestHelper::copyIntArray(arr1, n);
+    int* arr6 = SortTestHelper::copyIntArray(arr1, n);
 
     SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
     SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
     SortTestHelper::testSort("Quick Sort2", quickSort2, arr3, n);
     SortTestHelper::testSort("Heap Sort1", heapSort1, arr4, n);
     SortTestHelper::testSort("Heap Sort2", heapSort2, arr5, n);
+    SortTestHelper::testSort("Heap Sort", heapSort, arr6, n);
 
     delete[] arr1;
     delete[] arr2;
     delete[] arr3;
     delete[] arr4;
     delete[] arr5;
+    delete[] arr6;
     cout<<endl;
 
     int swapTimes = 100;
@@ -54,17 +85,20 @@ int main() {
     arr3 = SortTestHelper::copyIntArray(arr1, n);
     arr4 = SortTestHelper::copyIntArray(arr1, n);
     arr5 = SortTestHelper::copyIntArray(arr1, n);
+    arr6 = SortTestHelper::copyIntArray(arr1, n);
 
     SortTestHelper::testSort("Merge Sort", mergeSort, arr1, n);
     SortTestHelper::testSort("Quick Sort", quickSort, arr2, n);
     SortTestHelper::testSort("Quick Sort2", quickSort2, arr3, n);
     SortTestHelper::testSort("Heap Sort1", heapSort1, arr4, n);
     SortTestHelper::testSort("Heap Sort2", heapSort2, arr5, n);
+    SortTestHelper::testSort("Heap Sort", heapSort, arr6, n);
 
     delete[] arr1;
     delete[] arr2;
     delete[] arr3;
     delete[] arr4;
     delete[] arr5;
+    delete[] arr6;
     return 0;
 }
